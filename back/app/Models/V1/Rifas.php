d65e41c4df4814a72afa0da4cec4e9255e08d6d8
Victor Murilo <victor2706.vm@gmail.com>
2024-05-14 21:39:03 -0300

    campos novos colocados na model

--- back/app/Models/V1/Rifas.php
+++ back/app/Models/V1/Rifas.php
@@ -11,7 +11,14 @@ class Rifas extends Model
         "title", "description", "thumbnail", "rifa_status",
         "rifa_date", "price", "first_pacote_numbers", "first_pacote_discount",
         "second_pacote_numbers", "second_pacote_discount", "third_pacote_numbers", "third_pacote_discount", "fourth_pacote_numbers", "fourth_pacote_discount",
-        "rifa_numbers", "rifa_numbers_remaining",
+        "rifa_numbers", "rifa_numbers_remaining", "fifth_pacote_numbers",
+        "fifth_pacote_discount",
+        "sixth_pacote_numbers",
+        "sixth_pacote_discount",
+        "seventh_pacote_numbers",
+        "seventh_pacote_discount",
+        "eighth_pacote_numbers",
+        "eighth_pacote_discount"
     ];
 
 
