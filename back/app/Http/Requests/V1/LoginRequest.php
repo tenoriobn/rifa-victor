d4e483247bdcc9f17c04b620511e559124e27939
Victor Murilo <victor2706.vm@gmail.com>
2024-05-22 19:43:46 -0300

    tirando limite minimo na hora de logar

--- back/app/Http/Requests/V1/LoginRequest.php
+++ back/app/Http/Requests/V1/LoginRequest.php
@@ -23,7 +23,7 @@ class LoginRequest extends FormRequest
     {
         return [
             "email" => ["required", "email", "exists:users,email"],
-            "password" => ["required", "min:8", "max:255"],
+            "password" => ["required", "max:255"],
         ];
     }
 }
