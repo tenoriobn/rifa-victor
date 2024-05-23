a30977accf3e762357318300a28b073157d2a60f
Victor Murilo <victor2706.vm@gmail.com>
2024-05-22 23:07:49 -0300

    pegando números lá

--- back/app/Http/Controllers/V1/ClientController.php
+++ back/app/Http/Controllers/V1/ClientController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use \Exception;
 use App\Models\V1\Clients;
+use App\Models\V1\Cotas;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ItemNotFoundException;
 
@@ -19,7 +20,7 @@ class ClientController extends Controller
                 throw new ItemNotFoundException('Telefone não cadastrado');
             }
             $clientId = $client->id;
-            $info = DB::select("SELECT cotas.*, count(rifa_numbers.number) as numbers FROM rifa_numbers INNER JOIN cotas ON rifa_numbers.cota_id = cotas.id WHERE rifa_numbers.client_id = $clientId GROUP BY cotas.id");
+            $info = DB::select("SELECT cotas.id, cotas.payment_status, count(rifa_numbers.number) as numbers_quant, GROUP_CONCAT(rifa_numbers.number) as numbers FROM rifa_numbers INNER JOIN cotas ON rifa_numbers.cota_id = cotas.id WHERE rifa_numbers.client_id = $clientId GROUP BY cotas.id");
             return response()->json(["success" => true, "data" => $info], 200);
         } catch (ItemNotFoundException $e) {
             return response()->json(["success" => false, "msg" => $e->getMessage()], 404);
