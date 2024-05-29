c27e6f0c32cb2a453204d0a550952336df6605f9
Victor Murilo <victor2706.vm@gmail.com>
2024-05-29 13:47:51 -0300

    umas coisas que eu alterei e nem lembro mais

--- front-user/src/pages/home/components/Links.jsx
+++ front-user/src/pages/home/components/Links.jsx
@@ -5,13 +5,22 @@ import { useConfig } from '../../../context/ConfigContext'
 
 const Links = () => {
   const config = useConfig();
+  
+  function formatLink(link) {
+    if (!link.includes("https://")) {
+      return `https://${link}`
+    } else {
+      return link;
+    }
+  }
+
   return (
     <section className='mb-16'>
         <div className='flex justify-center gap-3'>
-            <a className='bg-[#E15426] p-3 w-10 rounded cursor-pointer' target='_blank' href={config.linkInsta ? config.linkInsta : "#"}>
+            <a className='bg-[#E15426] p-3 w-10 rounded cursor-pointer' target='_blank' href={config.linkInsta ? formatLink(config.linkInsta) : "#"}>
               <img src={insta} alt="" />
             </a>
-            <a className='bg-[#4CAF50] p-3 w-10 rounded cursor-pointer' target='_blank' href={config.linkWpp ? config.linkWpp : "#"}>
+            <a className='bg-[#4CAF50] p-3 w-10 rounded cursor-pointer' target='_blank' href={config.linkWpp ? formatLink(config.linkWpp) : "#"}>
               <img src={wpp} alt="" />
             </a>
         </div>
