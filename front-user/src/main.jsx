b6a13c767402b8732bed77e6e70abdc7b0c8809a
Bruno Tenório <91978811+tenoriobn@users.noreply.github.com>
2024-06-25 22:03:27 -0300

    Atualizando front-user

--- front-user/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./css/styles.css";
-import App from "./pages/App";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
