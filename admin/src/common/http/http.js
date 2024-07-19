01a9d8a5c4ecdd5e8a747bf9b7143bb234a66b9a
Alencar-Bianca <alencar-bianca@hotmail.com>
2024-07-19 00:04:54 -0300

    link de producao

--- admin/src/common/http/http.js
+++ admin/src/common/http/http.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://alimaprojetos.com/',
   headers: {
     'Content-Type': 'application/json',
   },
