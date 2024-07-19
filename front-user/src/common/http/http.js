4ea9ce8d28731734a95ca7cbe3d6d4082545c98b
Bruno Tenório <tenoriobn@gmail.com>
2024-07-19 19:28:13 -0300

    adicionando formatação nos inputs de telefone e configurando página de ganhadores

--- front-user/src/common/http/http.js
+++ front-user/src/common/http/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://alimaprojetos.com/',
+  baseURL: 'http://127.0.0.1:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
--- front-user/src/components/CardGanhadores/CardGanhadores.jsx
+++ front-user/src/components/CardGanhadores/CardGanhadores.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import Bilhete from "../../assets/Icons/bilhete.svg?react";
 import Calendario from "../../assets/Icons/calendario.svg?react";
-import { useRecoilState,  useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { estadoGanhadores, estadoProdutoSelecionado } from '../../common/state/atom';
 import { useEffect, useState } from 'react';
 import { fetchDados } from '../../common/http/http';
@@ -19,7 +19,7 @@ export default function CardGanhadores() {
     const obterDados = async () => {
       try {
         const dados = await fetchDados('/produtos/todos/ganhadores');
-        setGanhadores(dados.data);
+        setGanhadores(dados.data || []);
         setLoading(false);
       } catch (error) {
         setErroConexao(true);
@@ -42,54 +42,58 @@ export default function CardGanhadores() {
   }
 
   if (loading) {
-    return <AvisoCarregando /> 
+    return <AvisoCarregando />;
   }
-  
-  const ganhadoresFiltrados = ganhadores.filter(produto => produto.winner_id !== 0);
+
+  const ganhadoresFiltrados = (ganhadores || []).filter(produto => produto.winner_id !== 0);
 
   return (
     <>
-      {ganhadoresFiltrados.map(ganhadores => (
-        <div key={ganhadores.id} className="flex flex-col gap-4">
-          <Link 
-            className="flex w-auto overflow-hidden rounded-lg bg-neutral-200 hover:shadow-[4px_4px_4px_#0002] border border-solid border-neutral-400 ring-0 ring-amber-500/60 hover:ring-offset-4 hover:ring-2 transition-all duration-300"
-            to={`/${ganhadores.rifa.slug}/${ganhadores.rifa.id}`}
-            onClick={() => setProdutoSelecionado(ganhadores)}
-          >
-            <img 
-              className="w-[80px] m-2 rounded-lg object-cover transition-all" 
-              src={ganhadores.img}  
-              alt="Foto do ganhador"
-            />
+      {ganhadoresFiltrados.length > 0 ? (
+        ganhadoresFiltrados.map(ganhadores => (
+          <div key={ganhadores.id} className="flex flex-col gap-4">
+            <Link 
+              className="flex w-auto overflow-hidden rounded-lg bg-neutral-200 hover:shadow-[4px_4px_4px_#0002] border border-solid border-neutral-400 ring-0 ring-amber-500/60 hover:ring-offset-4 hover:ring-2 transition-all duration-300"
+              to={`/${ganhadores.rifa.slug}/${ganhadores.rifa.id}`}
+              onClick={() => setProdutoSelecionado(ganhadores)}
+            >
+              <img 
+                className="w-[80px] m-2 rounded-lg object-cover transition-all" 
+                src={ganhadores.img}  
+                alt="Foto do ganhador"
+              />
 
-            <div className="flex grow flex-col bg-neutral-100 overflow-hidden p-2">
-              <p className="text-lg w-full font-semibold text-neutral-900 text-left truncate">
-              {ganhadores.client.name}
-              </p>
+              <div className="flex grow flex-col bg-neutral-100 overflow-hidden p-2">
+                <p className="text-lg w-full font-semibold text-neutral-900 text-left truncate">
+                  {ganhadores.client.name}
+                </p>
 
-              <p className="text-sm w-full text-neutral-600 text-left truncate"> {ganhadores.rifa.description_resume}</p>
+                <p className="text-sm w-full text-neutral-600 text-left truncate">
+                  {ganhadores.rifa.description_resume}
+                </p>
 
-              <div className='flex items-center gap-2'>
-                <Bilhete className="icon text-emerald-500" />
-                <p className="text-sm text-neutral-600 text-left truncate">Bilhete da Sorte: </p>
-                <p className="text-sm font-semibold text-zinc-700">{ganhadores.ticket}</p>
+                <div className='flex items-center gap-2'>
+                  <Bilhete className="icon text-emerald-500" />
+                  <p className="text-sm text-neutral-600 text-left truncate">Bilhete da Sorte: </p>
+                  <p className="text-sm font-semibold text-zinc-700">{ganhadores.ticket}</p>
+                </div>
+
+                <div className='flex items-center gap-2'>
+                  <Calendario className="icon text-amber-500" />
+                  <p className="text-sm text-neutral-600 text-left truncate">Data do Sorteio: </p>
+                  <p className="text-sm font-semibold text-zinc-700">{ganhadores.draw_day}</p>
+                </div>
               </div>
-
-              <div className='flex items-center gap-2'>
-                <Calendario className="icon text-amber-500" />
-                <p className="text-sm text-neutral-600 text-left truncate">Data do Sorteio: </p>
-                <p className="text-sm font-semibold text-zinc-700">{ganhadores.draw_day}</p>
-              </div>
-            </div>
-          </Link>
-        </div>
-      ))}
-
-      <CardAviso
-        classes="border-sky-400 bg-sky-100 text-sky-400"
-        subtitulo="Nenhum ainda!"
-        mensagem=" Significa que você pode ser o primeiro!"
-      />
+            </Link>
+          </div>
+        ))
+      ) : (
+        <CardAviso
+          classes="border-sky-400 bg-sky-100 text-sky-400"
+          subtitulo="Nenhum ainda!"
+          mensagem="Significa que você pode ser o primeiro!"
+        />
+      )}
     </>
   );
 }
